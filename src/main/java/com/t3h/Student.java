212ab466c184d172d5c600d23496fd7e281a38ac
quyetcuong240797 <quyetcuongtrangnamm@gmail.com>
2018-05-12 18:41:46 -0700

    add getPassword for student

--- src/main/java/com/t3h/Student.java
+++ src/main/java/com/t3h/Student.java
@@ -13,10 +13,13 @@ public class Student {
         return username;
     }
 
-fhfgfgh
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
     }
 
 
