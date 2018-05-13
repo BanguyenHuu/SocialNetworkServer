372edb4fe6c5298a35ed23d2b7808c012117c386
BanguyenHuu <nguyenhuubavn@gmail.com>
2018-05-13 08:56:48 +0700

    no comment

--- src/main/resources/social.sql
+++ src/main/resources/social.sql
@@ -16,41 +16,85 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `friends`
+-- Table structure for table `friends`
 --
 
-LOCK TABLES `friends` WRITE;
-/*!40000 ALTER TABLE `friends` DISABLE KEYS */;
-/*!40000 ALTER TABLE `friends` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `friends`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `friends` (
+  `user1_id` varchar(15) NOT NULL,
+  `user2_id` varchar(15) NOT NULL,
+  `time_create` date DEFAULT NULL,
+  PRIMARY KEY (`user1_id`,`user2_id`),
+  KEY `fl_user2_friend_idx` (`user2_id`),
+  CONSTRAINT `fk_user1_friend` FOREIGN KEY (`user1_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fl_user2_friend` FOREIGN KEY (`user2_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `message`
+-- Table structure for table `message`
 --
 
-LOCK TABLES `message` WRITE;
-/*!40000 ALTER TABLE `message` DISABLE KEYS */;
-/*!40000 ALTER TABLE `message` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `message` (
+  `sender_id` varchar(15) NOT NULL,
+  `receiver_id` varchar(15) NOT NULL,
+  `send_tiem` date DEFAULT NULL,
+  `content` longtext,
+  `img` longtext,
+  `video` longtext,
+  KEY `fk_sender_id_idx` (`sender_id`),
+  KEY `fk-reciever_id_idx` (`receiver_id`),
+  CONSTRAINT `fk-reciever_id` FOREIGN KEY (`receiver_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_sender_id` FOREIGN KEY (`sender_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `post`
+-- Table structure for table `post`
 --
 
-LOCK TABLES `post` WRITE;
-/*!40000 ALTER TABLE `post` DISABLE KEYS */;
-/*!40000 ALTER TABLE `post` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `post_id` varchar(15) NOT NULL,
+  `userpost_id` varchar(15) DEFAULT NULL,
+  `time_post` date DEFAULT NULL,
+  `content` longtext,
+  `img` longtext,
+  `video` longtext,
+  PRIMARY KEY (`post_id`),
+  KEY `post_fk_idx` (`userpost_id`),
+  CONSTRAINT `post_fk` FOREIGN KEY (`userpost_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Table structure for table `user`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('us111','ba','199703','nguyenhuubavn@gmail.com',NULL,NULL,NULL,NULL,NULL,NULL);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` varchar(15) NOT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(15) DEFAULT NULL,
+  `email` longtext,
+  `avatar` longtext,
+  `birthday` date DEFAULT NULL,
+  `token` varchar(45) DEFAULT NULL,
+  `city` varchar(25) DEFAULT NULL,
+  `job` varchar(25) DEFAULT NULL,
+  `sex` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -61,4 +105,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-13  8:50:50
+-- Dump completed on 2018-05-13  8:56:16
